c5696d3f432a1606771480a65e6bc543bde659e6
desdrury <des@drury-family.com>
2016-07-08 15:33:28 +1000

    Updated OpenDJ run.sh to work with k8sdc volume

--- opendj/run.sh
+++ opendj/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This startup script is meant to be used in a Kubernetes environment
+# This startup script is meant to be used in a k8sdc Kubernetes environment
 # The idea is to consolidate all of the writable DJ directories to
 # a single directory root, and update DJ's instance.loc file to point to that root
 # This allows us to to mount a data volume on that root which will give us
@@ -8,15 +8,13 @@
 
 cd /opt/opendj
 
-# Instance.loc does not exist?
-if [ ! -e instance.loc ] ; then
-  mkdir instance
+if [ ! -d /opt/opendj/instance/config/ ] ; then
   mv config/ instance/
   mv db/ instance/
   mkdir instance/logs
   mkdir instance/locks
-  echo "./instance" > instance.loc
 fi
+echo "./instance" > instance.loc
 
 bin/start-ds
 
